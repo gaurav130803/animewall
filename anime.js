50a251d98df18a9fb9ff7f0b927ebeef4850eff1
gaurav130803 <123970142+gaurav130803@users.noreply.github.com>
2023-02-02 23:03:20 +0530

    Update anime.js

--- anime.js
+++ anime.js
@@ -1,10 +1,64 @@
 // swipper
 
 
-const swiper = new Swiper('.swiper', {
+var swiper = new Swiper('.swiper', {
     // Optional parameters
-    slidePerView:5,
-    spaceBetween:20,
+    slidesPerView: 1,
+    spaceBetween: 20,
+
+    autoplay :{
+        delay :3000,
+        disableOnInteraction : false,
+    },
+    //<!-- loop : true, -->
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable :true,
+    },
+  
+    // Navigation arrows
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
+  
+    
+  });
+
+
+  var swiper = new Swiper('.trending-swiper', {
+    // Optional parameters
+    slidesPerView: 5,
+    spaceBetween: 20,
+
+    autoplay :{
+        delay :3000,
+        disableOnInteraction : false,
+    },
+    //<!-- loop : true, -->
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable :true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.arrow-key-left1',
+      prevEl: '.arrow-key-right1',
+    },
+  
+    
+  });
+
+
+  var swiper = new Swiper('.trending-swiper-heading', {
+    // Optional parameters
+    slidesPerView: 5,
+    spaceBetween: 20,
 
     autoplay :{
         delay :3000,
